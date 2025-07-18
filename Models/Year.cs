cb1a7b05b37705bec88c612830743d3660219cff
EmilyEstrada <egrosch96@gmail.com>
2025-07-18 19:37:37 -0400

    App is working for student so far. table created

---
+++ Models/Year.cs
@@ -0,0 +1,9 @@
+namespace ChoirManager.Models
+{
+    public class Year
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ChoirManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ChoirDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -12,6 +17,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
